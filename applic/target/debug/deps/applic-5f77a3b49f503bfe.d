e45f5e187acefcac02ffcda9f9e31ba89d370960
s333770 <48990117+s333770@users.noreply.github.com>
2022-10-02 16:45:17 +0200

    Added Rust for Wasm

---
+++ applic/target/debug/deps/applic-5f77a3b49f503bfe.d
@@ -0,0 +1,5 @@
+/Users/andreastonsethmyhr/Development2.0/Bachelor2022-autumn/bachelor2022/applic/target/debug/deps/applic-5f77a3b49f503bfe.rmeta: src/lib.rs
+
+/Users/andreastonsethmyhr/Development2.0/Bachelor2022-autumn/bachelor2022/applic/target/debug/deps/applic-5f77a3b49f503bfe.d: src/lib.rs
+
+src/lib.rs:
---
+++ applic/target/debug/deps/applic-d8b951c72b9d56d0.d
@@ -0,0 +1,5 @@
+/Users/andreastonsethmyhr/Development2.0/Bachelor2022-autumn/bachelor2022/applic/target/debug/deps/applic-d8b951c72b9d56d0.rmeta: src/lib.rs
+
+/Users/andreastonsethmyhr/Development2.0/Bachelor2022-autumn/bachelor2022/applic/target/debug/deps/applic-d8b951c72b9d56d0.d: src/lib.rs
+
+src/lib.rs:
