6593ae576a35fdcb31357349518e343e2cfa8212
s333770 <48990117+s333770@users.noreply.github.com>
2022-10-02 16:44:41 +0200

    Done with animation window

--- src/drawing/Animation.tsx
+++ src/drawing/Animation.tsx
@@ -15,10 +15,28 @@ export const Animation = ({ width, height }: AnimationProps) => {
               context.beginPath();
               context.fillStyle="white"
               context.fillRect(0, 0, canvas.width, canvas.height);
-
               context.fill(); 
+
+              const renderFrame = () => {
+              context.beginPath();
+              context.arc(200, 75, 50, 0, 2 * Math.PI);
+              context.fillStyle="red";
+              context.stroke();
+              context.fill();
+                
+              };
+            
+              const tick = () => {
+                if (!canvasRef.current) return;
+                renderFrame();
+                requestAnimationFrame(tick);
+              };
+            
+     
+                requestAnimationFrame(tick);
           }
         }},[]);    
+        
 
     return <canvas ref={canvasRef} height={height} width={width} />
       }
